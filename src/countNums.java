aba1090b3bb0863bfd3f490738fbf580e793a222
Vikram Kumar <vsaw1921@gmail.com>
2024-11-05 23:12:29 +0530

    Initial commit

---
+++ src/countNums.java
@@ -0,0 +1,15 @@
+public class count {
+    public static void main(String[] args) {
+        int n = 477873;
+        int count = 0;
+        while(n > 0) {
+            int rem = n % 10;
+            if (rem==7) {
+             count++;
+            }
+            n = n / 10;
+            System.out.println(count);
+
+        }
+    }
+}
